6dd5fab28424a0230627d3d751a0c5ec30a4ce25
Dilpreet S. Chana <dschana6@gmail.com>
2017-12-30 03:51:17 -0500

    Basic setup

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+#include <cstdio>
 #include "opencv2/highgui.hpp"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/imgproc.hpp"
 
+#define FOCAL_LENGTH 0	       // Focal length of both camereas
+#define CAMERA_DIFFERENCE 0  // Distance between cameras
+
 using namespace cv;
+
+float findTarget(Mat &frame1, &frame2);
+double calculateDistance(float d);
 
 int main() {
 	VideoCapture cap1(0);
@@ -12,6 +19,8 @@ int main() {
 	if (!cap1.isOpened() || !cap2.isOpened()) return -1;
 
 	while (cap1.read(frame1) && cap2.read(frame2)) {
+		printf("%f\n", calculateDistance(findTarget(frame1, frame2)));
+
 		// Esc to end
 		if (char(waitKey(10)) == 27) {
 			break;
@@ -19,4 +28,12 @@ int main() {
 	}
 
 	return 0;
+}
+
+float findTarget(Mat &frame1, Mat &frame2) {
+
+}
+
+double calculateDistance(float d) {
+	return FOCAL_LENGTH * CAMERA_DIFFERENCE / d
 }
