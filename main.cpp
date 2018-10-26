8dcbf57f739e1b2dbf3669f190e3511298cc7d24
Dilpreet S. Chana <dschana6@gmail.com>
2018-10-26 14:14:40 -0400

    Full image depth map attemp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
  * Description:	Dual camera range finder
 **/
 
+#include <algorithm>
 #include <cstdio>
 #include <cmath>
 #include <vector>
-
-#include "opencv2/highgui.hpp"
-#include "opencv2/objdetect.hpp"
-#include "opencv2/imgproc.hpp"
+#include <initializer_list>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 // Camera properties (Microsoft HD 3000)
 #define FOCAL_LENGTH 60
@@ -19,7 +19,10 @@
 #define LIGHT_SENSOR_WIDTH 17.3
 #define LIGHT_SENSOR_HEIGHT 13
 
+#define TILE_SIZE 2
 #define FACE_CASCADE "data/haarcascade_frontalface_alt.xml"
+#define MAX_DIST 4000
+#define THRESHOLD 100
 
 using namespace cv;
 using namespace std;
@@ -27,14 +30,15 @@ using namespace std;
 // Global constants
 CascadeClassifier face_cascade;
 
-double calculateDeviation(Mat& frame1, Mat& frame2, VideoCapture c);
-Point findPattern(Mat& frame);
-double calculateDistance(float d);
+double calculateDistance(Point p1, Point p2, VideoCapture c);
 
 int main() {
-	VideoCapture cap1(0);
-	VideoCapture cap2(1);
-	Mat frame1, frame2;
+	VideoCapture cap1("data/frame_left.jpg");
+	VideoCapture cap2("data/frame_right.jpg");
+	//VideoCapture cap1(0);
+	//VideoCapture cap2(1);
+	Mat frame1, frame2, depth_map;
+	Mat edge1, edge2;
 
 	if (!cap1.isOpened()) {
 		printf("Cap1 fucked\n");
@@ -46,30 +50,59 @@ int main() {
 		return -1;
 	}
 
-	if (!face_cascade.load(FACE_CASCADE)) {
-		printf("Cascade fucked\n");
-		return -1;
-	}
-
 	while (cap1.read(frame1) && cap2.read(frame2)) {
-		printf("%f mm\n", calculateDistance(calculateDeviation(frame1, frame2, cap1)));
+		Canny(frame1, edge1, 50, 100);
+		Canny(frame2, edge2, 50, 100);
+
+		depth_map = Mat(edge1.rows, edge1.cols, CV_8UC1, Scalar(0));
+
+		for (int y = 0; y < edge2.rows - TILE_SIZE; y += TILE_SIZE) {
+			for (int x = 0; x < edge2.cols - TILE_SIZE; x += TILE_SIZE) {
+				Mat p1(edge2, Rect(x, y, TILE_SIZE, TILE_SIZE));
+
+				for (int tx = x; tx < edge1.cols - TILE_SIZE; tx += TILE_SIZE) {
+					Mat p2(edge1, Rect(tx, y, TILE_SIZE, TILE_SIZE));
+
+					// Compare p1 and p2
+					Mat diff = p1 - p2;
+					int comp = 0;
+
+					for (int i = 0; i < diff.rows; i++) {
+						for (int j = 0; j < diff.cols; j++) {
+							comp += diff.at<uchar>(i, j) * diff.at<uchar>(i, j);
+						}
+					}
+
+					comp = sqrt(comp);
+
+					if (comp < THRESHOLD) {
+						double dist = calculateDistance(Point(x, y), Point(tx, y), cap1);
+						rectangle(depth_map, Rect(x, y, TILE_SIZE, TILE_SIZE), Scalar(dist / MAX_DIST * 255), CV_FILLED);
+
+						break;
+					}
+				}
+			}
+		}
 
 		imshow("Cam1", frame1);
 		imshow("Cam2", frame2);
+		imshow("Edge1", edge1);
+		imshow("Edge2", edge2);
+		imshow("Depth Map", depth_map);
 
 		// Esc to end
 		if (char(waitKey(10)) == 27) {
 			break;
 		}
+
+		while (true) {}
 	}
 
 	return 0;
 }
 
-double calculateDeviation(Mat& frame1, Mat& frame2, VideoCapture c) {
-	Point p1 = findPattern(frame1);
-	Point p2 = findPattern(frame2);
-
+double calculateDistance(Point p1, Point p2, VideoCapture c) {
 	if ((p1.x == 0 && p1.y == 0) || (p2.x == 0 && p2.y == 0)) return 0;
 
 	// Frames need to swap
@@ -85,37 +118,5 @@ double calculateDeviation(Mat& frame1, Mat& frame2, VideoCapture c) {
 	double s1 = (LIGHT_SENSOR_WIDTH / 2) * (p1.x - c.get(CV_CAP_PROP_FRAME_WIDTH) / 2) / (c.get(CV_CAP_PROP_FRAME_WIDTH) / 2);  // -DF
 	double s2 = (LIGHT_SENSOR_WIDTH / 2) * (p2.x - c.get(CV_CAP_PROP_FRAME_WIDTH) / 2) / (c.get(CV_CAP_PROP_FRAME_WIDTH) / 2);  // EG
 
-	return s2 - s1;
-}
-
-Point findPattern(Mat& frame) {
-	// Perhaps find faces for round 1
-	vector<Rect> faces;
-	Mat frame_gray, frame_gray_blur;
-
-	cvtColor(frame, frame_gray, CV_BGR2GRAY);
-	equalizeHist(frame_gray, frame_gray);
-
-	GaussianBlur(frame_gray, frame_gray_blur, Size(3, 3), 0, 0);
-
-	Rect main_face = Rect(0, 0, 0, 0);
-
-	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(80, 80));
-
-	for (size_t i = 0; i < faces.size(); i++) {
-		if (faces[i].width * faces[i].height > main_face.width * main_face.height) {
-			main_face.x = faces[i].x;
-			main_face.y = faces[i].y;
-			main_face.width = faces[i].width;
-			main_face.height = faces[i].height;
-		}
-	}
-
-	rectangle(frame, main_face, Scalar(155, 155, 155), 5);
-
-	return Point(main_face.x + main_face.width / 2, main_face.y + main_face.height / 2);
-}
-
-double calculateDistance(float d) {
-	return FLANGE_FOCAL_DISTANCE * CAMERA_DIFFERENCE / d;
+	return FLANGE_FOCAL_DISTANCE * CAMERA_DIFFERENCE / (s2 - s1);
 }
